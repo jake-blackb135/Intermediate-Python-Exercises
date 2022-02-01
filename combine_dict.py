c0d1f4879743dca26592b34ddf8bfc4734ed93c1
jake-blackb135 <jake@choosecarolinas.com>
2022-02-01 15:11:42 -0500

    Adding comments

--- combine_dict.py
+++ combine_dict.py
@@ -1,15 +1,20 @@
+#function to combine common elements of 2 dictionaries
 def get_combined_dict(my_dict1,my_dict2):
     new_dict = {}
+    #if 1st dict is longer than or equal to second loop through every element of first dict
+    #and combine like elements
     if len(my_dict1) >= len(my_dict2):
         for x in my_dict1.keys():
             if x in my_dict2:
                 new_dict.update({x:my_dict1[x]+my_dict2[x]})
+    #If second dict is longer loop through second dict and combine like elements
     else:
         for x in my_dict2.keys():
             if x in my_dict1:
                 new_dict.update({x:my_dict1[x]+my_dict2[x]})
     return new_dict
 
+#testing get_combined_dict method
 my_dict_1 = {'a': 5, 'b': 12, 'c': 3, 'd': 9}
 my_dict_2 = {'b': 4, 'c': 9, 'd': 10, 'e': 16}
 combined_dict = get_combined_dict(my_dict_1, my_dict_2)
